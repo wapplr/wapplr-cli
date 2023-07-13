435ab8f084b82f68b3e68919571bb5e5823f1373
C4RC0 <karesz.carco@gmail.com>
2023-07-13 17:45:22 +0200

    postcss-scss loader

--- packages/postcss-config-wapplr/index.js
+++ packages/postcss-config-wapplr/index.js
@@ -1,7 +1,15 @@
 const postcss = require("postcss");
+const postcssScss = require("postcss-scss");
 const getPlugins = require("./plugins");
 const plugins = getPlugins();
-const runner = postcss(plugins);
+
+const r = postcss(plugins);
+const runner = {
+    ...r,
+    process: async (css, options = {}, ...attr)=>{
+        return await r.process(css, {...options, syntax: postcssScss}, ...attr)
+    }
+};
 
 module.exports = async function (processCss) {
     return await processCss({postcss, plugins, runner});
